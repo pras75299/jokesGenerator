9a2a55cdc8eef659652ba97c507f7a275baa7b18
pras75299 <singh.prashantking@gmail.com>
2020-10-31 23:51:45 +0530

    add logic to app js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 const API_URL = `http://api.icndb.com/jokes/random`;
 
 function App() {  
+  const [joke, setJoke] = useState('');
+  const generateJoke = () => {
+    fetch(API_URL)
+      .then(response => response.json())
+      .then(data => setJoke(data.value.joke));
+  }
+  useEffect(() => {
+    generateJoke();
+  }, [])
   return (
     <div className="box">
       <h2>Jokes Generator</h2>
-      <p>Here comes the jokes</p>
-      <button>Get the Joke</button>
+      <p>{joke}</p>
+      <button onClick={generateJoke}>Get the Joke</button>
     </div>
   );
 }
